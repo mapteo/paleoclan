6092a05d14c13685f1f0bd73d8864a6d062139f8
Matteo Zobbi <matteo@mikamai.com>
2013-01-26 18:14:57 +0100

    test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '253926028072270', 'ef232588e7f5ec9a9dd3a068c19198ce'
-end
+# OmniAuth.config.logger = Rails.logger
+# 
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider :facebook, '253926028072270', 'ef232588e7f5ec9a9dd3a068c19198ce'
+# end
